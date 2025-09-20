825fb94a33f10e62a54026204a458e75c484fc57
Dhruvbindal288 <dhruvbindal288@gmail.com>
2025-09-21 00:28:42 +0530

    final

--- backend/server.js
+++ backend/server.js
@@ -1,25 +1,22 @@
-
-
 import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
 import path from "path"; 
 
-import authRoutes from "./src/routes/auth.route.js";
-import diaryRoutes from "./src/routes/diary.route.js";
-import chatRoutes from "./src/routes/chat.route.js";
-import connectDB from "./src/lib/db.js";
+import authRoutes from "./routes/auth.route.js";
+import diaryRoutes from "./routes/diary.route.js";
+import chatRoutes from "./routes/chat.route.js";
+import connectDB from "./db/db.js";
+
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-
 const __dirname = path.resolve();
 
-
 const corsOptions = {
-  origin: ['http://localhost:3000'], 
+  origin: ['http://localhost:3000', 'https://calmspace-vjkj.onrender.com'], 
   credentials: true,
 };
 app.use(cors(corsOptions));
@@ -31,13 +28,12 @@ app.use("/api/diary", diaryRoutes);
 app.use("/api/chat", chatRoutes);
 
 
-app.use(express.static(path.join(__dirname, "/frontend/dist")));
+app.use(express.static(path.join(__dirname, "frontend", "dist")));
 
-//Catching all route to build react app
+
 app.get(/^(?!\/api).*/, (req, res) => {
   res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
 });
-
 
 app.listen(PORT, () => {
   connectDB();
