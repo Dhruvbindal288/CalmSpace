b843d993d3cf2cbd66a31b4e9d68ef727a5b6c36
Dhruvbindal288 <dhruvbindal288@gmail.com>
2025-09-20 23:58:40 +0530

    Change package file

--- backend/server.js
+++ backend/server.js
@@ -1,31 +1,46 @@
+// In backend/server.js
+
 import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
-import connectDB from "./src/lib/db.js";
+import path from "path"; 
+
+import authRoutes from "./routes/auth.route.js";
+import diaryRoutes from "./routes/diary.route.js";
+import chatRoutes from "./routes/chat.route.js";
+import connectToMongoDB from "./db/connectToMongoDB.js";
+
 dotenv.config();
-import authRouter from './src/routes/auth.route.js'
-import diaryRouter from './src/routes/diary.route.js'
-import moodRouter from './src/routes/mood.route.js'
-import quoteRouter from './src/routes/quote.route.js'
-import chatRouter from './src/routes/chat.route.js'
-import cookieParser from "cookie-parser";
 
 const app = express();
-
-
-app.use(cors({origin:'http://localhost:5173',credentials:true}));
-app.use(express.json());
-app.use(cookieParser())
-app.use('/api/auth',authRouter)
-app.use('/api/diary',diaryRouter)
-app.use('/api/mood',moodRouter)
-app.use('/api/quote',quoteRouter)
-app.use('/api/chat',chatRouter)
-
-
-connectDB();
-
 const PORT = process.env.PORT || 5000;
+
+
+const __dirname = path.resolve();
+
+
+const corsOptions = {
+  origin: ['http://localhost:3000'], 
+  credentials: true,
+};
+app.use(cors(corsOptions));
+app.use(express.json());
+
+
+app.use("/api/auth", authRoutes);
+app.use("/api/diary", diaryRoutes);
+app.use("/api/chat", chatRoutes);
+
+
+app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+//Catching all route to build react app
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+});
+
+
 app.listen(PORT, () => {
-  console.log(`🚀 Server running on port ${PORT}`);
+  connectToMongoDB();
+  console.log(`Server running on port ${PORT}`);
 });
